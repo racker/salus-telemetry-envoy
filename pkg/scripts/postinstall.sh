b6386ae4977ca51324e670f8ebd2707c12cd67ab
Geoff Bourne <geoff.bourne@rackspace.com>
2019-09-11 09:17:37 -0500

    Run packaged envoy as telemetry-envoy

--- pkg/scripts/postinstall.sh
+++ pkg/scripts/postinstall.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 setcap CAP_SETFCAP+p /usr/local/bin/telemetry-envoy
+
+# Only adjust top level of data directory since just need to fix up initial, empty dir.
+# A recursive chown on a fully populated data directory could become time consuming.
+chown telemetry-envoy: /var/lib/telemetry-envoy
+
+# telemetry-envoy owns config
+chown -R telemetry-envoy: /etc/salus
+# ...and only telemetry-envoy can access config, since there are sensitive fields
+chmod -R go-rwx /etc/salus
