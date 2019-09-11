f42a3481e14bdec2b12558caeb3da6f6b6b2cbaa
Geoff Bourne <geoff.bourne@rackspace.com>
2019-09-11 14:47:32 -0500

    Add preinstall script to goreleaser

--- pkg/scripts/postinstall.sh
+++ pkg/scripts/postinstall.sh
@@ -4,9 +4,9 @@ setcap CAP_SETFCAP+p /usr/local/bin/telemetry-envoy
 
 # Only adjust top level of data directory since just need to fix up initial, empty dir.
 # A recursive chown on a fully populated data directory could become time consuming.
-chown telemetry-envoy: /var/lib/telemetry-envoy
+chown telemetry-envoy:telemetry-envoy /var/lib/telemetry-envoy
 
 # telemetry-envoy owns config
-chown -R telemetry-envoy: /etc/salus
+chown -R telemetry-envoy:telemetry-envoy /etc/salus
 # ...and only telemetry-envoy can access config, since there are sensitive fields
 chmod -R go-rwx /etc/salus
--- pkg/scripts/preinstall.sh
+++ pkg/scripts/preinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if ! id telemetry-envoy &>/dev/null; then
-    useradd -r -M telemetry-envoy -s /bin/false -d /var/lib/telemetry-envoy
+    useradd -r -M telemetry-envoy -s /bin/false -d /var/lib/telemetry-envoy -U
 fi
