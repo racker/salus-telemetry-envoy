9b7ff767eed1c7ae33de19441d1ac4700a2935f2
Geoff Bourne <geoff.bourne@rackspace.com>
2019-09-12 09:21:22 -0500

    Use postinstall to adapt systemd file location to distro

--- pkg/scripts/postinstall.sh
+++ pkg/scripts/postinstall.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
+USER=telemetry-envoy
+GROUP=telemetry-envoy
+SCRIPT_DIR=/usr/lib/telemetry-envoy/scripts
+ETC_DIR=/etc/salus
+
 setcap CAP_SETFCAP+p /usr/local/bin/telemetry-envoy
 
 # Only adjust top level of data directory since just need to fix up initial, empty dir.
 # A recursive chown on a fully populated data directory could become time consuming.
-chown telemetry-envoy:telemetry-envoy /var/lib/telemetry-envoy
+chown ${USER}:${GROUP} /var/lib/telemetry-envoy
 
 # telemetry-envoy owns config
-chown -R telemetry-envoy:telemetry-envoy /etc/salus
+chown -R ${USER}:${GROUP} ${ETC_DIR}
 # ...and only telemetry-envoy can access config, since there are sensitive fields
-chmod -R go-rwx /etc/salus
+chmod -R go-rwx ${ETC_DIR}
+
+if [ -d /lib/systemd ]; then
+  # Debian/Ubuntu
+  SYSTEMD_PATH=/lib/systemd
+elif [ -d /usr/lib/systemd ]; then
+  # Redhat
+  SYSTEMD_PATH=/usr/lib/systemd
+else
+  echo "ERROR: unable to detect SYSTEMD_PATH"
+  exit 1
+fi
+
+cp -f ${SCRIPT_DIR}/*.service ${SYSTEMD_PATH}/system/
+cp -f ${SCRIPT_DIR}/*.preset ${SYSTEMD_PATH}/system-preset/
+
+systemctl preset
+systemctl daemon-reload
