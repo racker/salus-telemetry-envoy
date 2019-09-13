b0fd327be55494d1258f52f8b1d8eab3d652f827
Geoff Bourne <geoff.bourne@rackspace.com>
2019-09-13 11:20:27 -0500

    Stopping service in preremove was interfering with restart on upgrade

--- pkg/scripts/preremove.sh
+++ pkg/scripts/preremove.sh
@@ -2,4 +2,12 @@
 
 SERVICE=telemetry-envoy
 
-systemctl stop ${SERVICE}.service
+
+# debs pass upgrade, remove, or purge
+# rpms pass number of versions installed, so 0 means none remain
+if [ "$1" == "remove" ] || [ "$1" == "purge" ] || [ "$1" == "0" ]; then
+
+  systemctl stop ${SERVICE}.service
+
+fi
+
