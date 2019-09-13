a373320308f4f4f1748a76f3be34baa46404356f
Geoff Bourne <geoff.bourne@rackspace.com>
2019-09-13 09:52:23 -0500

    On purge, remove all of /etc/salus

--- pkg/scripts/postremove.sh
+++ pkg/scripts/postremove.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SERVICE=telemetry-envoy
+ETC_DIR=/etc/salus
 
 if [ -d /lib/systemd ]; then
   # Debian/Ubuntu
@@ -14,11 +15,15 @@ else
 fi
 
 # debs pass upgrade, remove, or purge
-# rpms pass number of versions installed, so 0 means none
+# rpms pass number of versions installed, so 0 means none remain
 if [ "$1" == "remove" ] || [ "$1" == "purge" ] || [ "$1" == "0" ]; then
   systemctl disable ${SERVICE}
 
   rm -f ${SYSTEMD_PATH}/system/${SERVICE}.service
 
   systemctl daemon-reload
+fi
+
+if [ "$1" == "purge" ]; then
+  rm -rf ${ETC_DIR}
 fi
