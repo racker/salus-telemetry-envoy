05aa83cdc4331458b50126d9b032dd6d2575692b
Geoff Bourne <geoff.bourne@rackspace.com>
2019-10-10 09:13:55 -0500

    Detect systemd before performing service install and systemctl calls (#66)

--- pkg/scripts/postinstall.sh
+++ pkg/scripts/postinstall.sh
@@ -7,6 +7,31 @@ DATA_DIR=/var/lib/telemetry-envoy
 ETC_DIR=/etc/salus
 SERVICE=telemetry-envoy
 
+function uses_systemd {
+  [[ "$(readlink /proc/1/exe)" == */systemd ]]
+}
+
+function post_install_systemd {
+  if [ -d /lib/systemd ]; then
+    # Debian/Ubuntu
+    SYSTEMD_PATH=/lib/systemd
+  elif [ -d /usr/lib/systemd ]; then
+    # Redhat
+    SYSTEMD_PATH=/usr/lib/systemd
+  else
+    echo "ERROR: unable to detect SYSTEMD_PATH"
+    exit 1
+  fi
+
+  cp -f ${SCRIPT_DIR}/${SERVICE}.service ${SYSTEMD_PATH}/system/
+
+  # load/reload service unit
+  systemctl daemon-reload
+
+  # for upgrades, restart service...only if already active
+  systemctl try-restart ${SERVICE}
+}
+
 setcap CAP_SETFCAP+p /usr/local/bin/telemetry-envoy
 
 # Only adjust top level of data directory since just need to fix up initial, empty dir.
@@ -18,21 +43,7 @@ chown -R ${USER}:${GROUP} ${ETC_DIR}
 # ...and only telemetry-envoy can access config, since there are sensitive fields
 chmod -R go-rwx ${ETC_DIR}
 
-if [ -d /lib/systemd ]; then
-  # Debian/Ubuntu
-  SYSTEMD_PATH=/lib/systemd
-elif [ -d /usr/lib/systemd ]; then
-  # Redhat
-  SYSTEMD_PATH=/usr/lib/systemd
-else
-  echo "ERROR: unable to detect SYSTEMD_PATH"
-  exit 1
+if uses_systemd; then
+  post_install_systemd
 fi
-
-cp -f ${SCRIPT_DIR}/${SERVICE}.service ${SYSTEMD_PATH}/system/
-
-# load/reload service unit
-systemctl daemon-reload
-
-# for upgrades, restart service...only if already active
-systemctl try-restart ${SERVICE}
+# else manual service setup is needed...for now
--- pkg/scripts/preremove.sh
+++ pkg/scripts/preremove.sh
@@ -2,12 +2,22 @@
 
 SERVICE=telemetry-envoy
 
+function uses_systemd {
+  [[ "$(readlink /proc/1/exe)" == */systemd ]]
+}
+
+function pre_remove_systemd {
+    systemctl stop ${SERVICE}.service
+}
 
 # debs pass upgrade, remove, or purge
 # rpms pass number of versions installed, so 0 means none remain
 if [ "$1" == "remove" ] || [ "$1" == "purge" ] || [ "$1" == "0" ]; then
 
-  systemctl stop ${SERVICE}.service
+  if uses_systemd; then
+    pre_remove_systemd
+  fi
+  # else manual service stopping is needed
 
 fi
 
