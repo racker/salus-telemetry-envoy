b422a6b2ad3f37ff70e7ec739bb972cd9e6f31b6
George Jahad <github@blackbirdsystems.net>
2019-11-22 17:53:06 -0800

    simple ingestor working

--- ingest/perf_test_ingestor.go
+++ ingest/perf_test_ingestor.go
@@ -17,137 +17,57 @@
 package ingest
 
 import (
-	"bufio"
 	"context"
-	"encoding/json"
-	"github.com/pkg/errors"
 	"github.com/racker/salus-telemetry-protocol/telemetry_edge"
 	"github.com/racker/telemetry-envoy/ambassador"
-	"github.com/racker/telemetry-envoy/config"
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
-	"net"
+	"time"
 )
 
-const (
-	telegrafJsonMetricChanSize = 10
-)
-
-type TelegrafJson struct {
-	listener   net.Listener
-	metrics    chan *telegrafJsonMetric
+type PerfTestIngestor struct {
 	egressConn ambassador.EgressConnection
 }
 
-type telegrafJsonMetric struct {
-	Timestamp int64
-	Name      string
-	Tags      map[string]string
-	Fields    map[string]interface{}
-}
-
 func init() {
-	viper.SetDefault(config.IngestTelegrafJsonBind, "localhost:8094")
-
-	registerIngestor(&TelegrafJson{})
+	registerIngestor(&PerfTestIngestor{})
 }
 
-func (t *TelegrafJson) Bind(conn ambassador.EgressConnection) error {
-	bind := viper.GetString(config.IngestTelegrafJsonBind)
-
-	listener, err := net.Listen("tcp", bind)
-	if err != nil {
-		return errors.Wrap(err, "failed to bind telegraf json listenener")
-	}
-
-	t.listener = listener
-	t.egressConn = conn
-
+func (p *PerfTestIngestor) Bind(conn ambassador.EgressConnection) error {
+	p.egressConn = conn
 	return nil
 }
 
-func (t *TelegrafJson) Start(ctx context.Context) {
-
-	t.metrics = make(chan *telegrafJsonMetric, telegrafJsonMetricChanSize)
-	go t.acceptConnections(ctx)
-
+func (p *PerfTestIngestor) Start(ctx context.Context) {
+	ticker := time.NewTicker(15 * time.Second)
 	for {
 		select {
 		case <-ctx.Done():
-			t.listener.Close()
+			ticker.Stop()
 			return
 
-		case m := <-t.metrics:
-			t.processMetric(m)
+		case <-ticker.C:
+			p.processMetric()
 		}
 	}
 }
 
-func (t *TelegrafJson) acceptConnections(ctx context.Context) {
-	for {
-		conn, err := t.listener.Accept()
-		if err != nil {
-			// errors during accept usually just mean the listener is closed
-			log.WithError(err).Debug("error while accepting telegraf ingest egressConn")
-			return
-		} else {
-			go t.handleConnection(ctx, conn)
-		}
-	}
-}
-
-func (t *TelegrafJson) handleConnection(ctx context.Context, conn net.Conn) {
-	log.WithField("addr", conn.RemoteAddr()).Info("handling telegraf json connection")
-
-	defer conn.Close()
-
-	scanner := bufio.NewScanner(conn)
-
-	for scanner.Scan() {
-		var m telegrafJsonMetric
-
-		content := scanner.Bytes()
-		if len(content) > 0 {
-			err := json.Unmarshal(content, &m)
-			if err != nil {
-				log.WithError(err).WithField("content", string(content)).Warn("failed to decode telegraf json metric")
-			} else {
-				log.WithField("m", &m).Debug("successfully unmarshaled json metric")
-				t.metrics <- &m
-			}
-		}
-	}
-
-	if scanner.Err() != nil {
-		log.WithError(scanner.Err()).Warn("failure while reading json lines")
-	}
-}
-
-func (t *TelegrafJson) processMetric(m *telegrafJsonMetric) {
-	log.WithField("m", m).Debug("processing metric")
+func (p *PerfTestIngestor) processMetric() {
 	fvalues := make(map[string]float64)
 	svalues := make(map[string]string)
-
-	for name, value := range m.Fields {
-		switch v := value.(type) {
-		case float64:
-			fvalues[name] = v
-		case string:
-			svalues[name] = v
-		}
-	}
-
+	tags := make(map[string]string)
+	tags["test_tag"] = "perfTestTag"
+	svalues["result_type"] = "success"
+	fvalues["result_code"] = -2.0
 	outMetric := &telemetry_edge.Metric{
 		Variant: &telemetry_edge.Metric_NameTagValue{
 			NameTagValue: &telemetry_edge.NameTagValueMetric{
-				Name:      m.Name,
-				Timestamp: m.Timestamp,
-				Tags:      m.Tags,
+				Name:      "perfTestMetric",
+				Timestamp: time.Now().Unix(),
+				Tags:      tags,
 				Fvalues:   fvalues,
 				Svalues:   svalues,
 			},
 		},
 	}
 
-	t.egressConn.PostMetric(outMetric)
+	p.egressConn.PostMetric(outMetric)
 }
