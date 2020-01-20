f2805d07e32b26d4889cfbec18998b1daecfb29b
Geoff Bourne <geoff.bourne@rackspace.com>
2020-01-20 15:09:35 -0600

    Refactored line protocol parsing to its own package

--- lineproto/influx_line_protocol_test.go
+++ lineproto/influx_line_protocol_test.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Rackspace US, Inc.
+ * Copyright 2020 Rackspace US, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package agents
+package lineproto
 
 import (
 	"github.com/racker/salus-telemetry-protocol/telemetry_edge"
